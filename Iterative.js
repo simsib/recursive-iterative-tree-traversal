7ed00b9ed6c8f19ef216b6460a7b87c5454270b7
Vydmantas Kolduba <vydmantas.kolduba@storebrand.no>
2017-03-09 12:14:33 +0200

    Iterative method

---
+++ Iterative.js
@@ -0,0 +1,27 @@
+const data = require('./DataFeed.js');
+
+let stack = [];
+function IterateOverTree(root) {
+    let level = 0;
+    root = {node:root,level:0};
+    while (root !== undefined) {
+        console.log(FormatValue(root.node.value,root.level));
+
+        root.node.nodes.forEach((node=>{
+            stack.push({node:node,level:root.level+1});
+        }));
+        root = stack.pop();
+    }
+};
+
+function FormatValue(value, level) {
+    let tmp = "";
+    for (let i = level; i > 0; i--) {
+        tmp += "  ";
+    }
+    return `${tmp} - ${value}`;
+}
+
+// MAIN
+
+IterateOverTree(data);
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # recursive-iterative-tree-traversal
+for recursive run 
+node Recursive.js 
+or for iterative run
+node Iterative.js
