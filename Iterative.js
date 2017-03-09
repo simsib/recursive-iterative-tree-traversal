d738cd3b643c1d7eff9400ab2075e067d8823979
weedma <vydmantas.kolduba@gmail.com>
2017-03-09 16:35:03 +0200

    refactored a bit (less code in iterative method)

--- Iterative.js
+++ Iterative.js
@@ -1,22 +1,17 @@
 const data = require('./DataFeed.js');
 const helper = require('./Helper.js');
-console.log(helper);
 let stack = [];
 function IterateOverTree(root) {
     let level = 0;
     root = {node:root,level:0};
     while (root !== undefined) {
         console.log(helper.FormatValue(root.node.value,root.level));
-        let tmp = [];
-        root.node.nodes.forEach((node=>{
-            tmp.push({node:node,level:root.level+1});
+        root.node.nodes.reverse().forEach((node=>{
+            stack.push({node:node,level:root.level+1});
         }));
-        // because of reversing nodes
-        stack.push(...tmp.reverse());
         root = stack.pop();
     }
 };
-
 
 // MAIN
 IterateOverTree(data);
